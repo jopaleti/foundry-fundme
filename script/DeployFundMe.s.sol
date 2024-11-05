57077d6ee39c71b1f4bac72c2b10f27b5a0b542f
clear <oopaletijohnson@gmail.com>
2024-11-05 18:54:23 +0100

    second commit

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {FundMe} from "../src/fundMe.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
 
     function run() external returns (FundMe) {
+        // Before startBroadcast => Not a `real` transaction, it simulate it in a simulated environment.
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(address(0));
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,14 +1,21 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
+
+    address USER = makeAddr("user");
+    uint256 constant SEND_VALUE = 0.1 ether;
+
     function setUp() external {
-        address priceFeedAddress = vm.envAddress("ethUsdPriceFeed");
-        fundMe = new FundMe(priceFeedAddress);
+        // address priceFeedAddress = vm.envAddress("ethUsdPriceFeed");
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        // fundMe = new FundMe(priceFeedAddress);
     }
 
     function testMinimumDollarIsFive() public {
@@ -16,12 +23,25 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.getOwner(), address(this));
+        assertEq(fundMe.getOwner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
         uint256 version = fundMe.getVersion();
         console.log(version);
-        assertEq(version, 4);
+        assertEq(version, 6);
+    }
+
+    function testFundFailsWithoutEnoughETH() public {
+        vm.expectRevert(); // Next line should revert
+        fundMe.fund(); // send 0 value
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next TX will be sent by USER
+        fundMe.fund{value: 10e18}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, 10e18);
     }
 }
